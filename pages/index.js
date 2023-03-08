4b655043671ac9ea55fb675f55b31d096a2567ee
Kehinde Otesile <kennyotesile@gmail.com>
2023-03-08 17:22:13 +0100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { DM_Sans } from '@next/font/google'
 import Image from 'next/image'
+import Nav from '@/components/Nav.js'
 
 const dmSans = DM_Sans({
     weight: ['400', '700'],
@@ -17,7 +18,7 @@ export default function Home() {
             </Head>
             
             <div>
-              <Image src="/logo.svg" width={500} height={500} />
+                <Nav />
             </div>
         </>
     );
