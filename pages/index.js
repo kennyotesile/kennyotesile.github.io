da7c0284155d40ba040c328e2f49b654064d33aa
Kehinde Otesile <kennyotesile@gmail.com>
2023-03-29 18:14:15 +0100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -31,19 +31,19 @@ export default function Home() {
                         <p className="text-gray-600 dark:text-gray-400 text-center">Ecommerce, CMS, CRM &nbsp;•&nbsp; Java, Spring, JavaScript, React (Next.js), AWS, etc.</p>
                         <br />
                         <div className="text-center mt-4">
-                            <Link href="/works/unulax-business" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                            <Link href="/works/unulax-business" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                         </div>
                     </div>
                     <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                 </section>
 
                 <section id="works_products" className="flex flex-col py-[48px] pb-[56px] px-[24px] md:px-[132px] gap-[32px] bg-black dark:bg-white text-white dark:text-black">
-                    <div id="about" className="pb-[32px] text-gray-200 dark:text-gray-800 text-3xl md:text-4xl font-medium w-full md:w-[80%]">
+                    <div id="about" className="pb-[32px] text-gray-200 dark:text-gray-800 text-3xl md:text-4xl font-bold w-full md:w-[80%]">
                     I am Kenny Otesile. I am a self-taught computer scientist, AI software engineer, and entrepreneur. I, with teams of designers and other software engineers, designed and developed Unulax Business, Amobern App, Amobern Pay, and other enterprise projects.
                     </div>
 
                     <div className="flex justify-between">
-                        <p className="text-lg font-medium">WORKS</p>
+                        <p className="text-lg font-bold">WORKS</p>
                         <Link href="#">More works</Link>
                     </div>
 
@@ -51,34 +51,34 @@ export default function Home() {
                         <div className="w-full md:w-1/3">
                             <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             <br />
-                            <h2 className="font-medium text-2xl">Amobern – a sophisticated ecommerce app</h2>
+                            <h2 className="font-bold text-2xl">Amobern – a sophisticated ecommerce app</h2>
                             <br />
                             <p className="text-gray-200 dark:text-gray-500">Ecommerce &nbsp;•&nbsp; Java, Spring, Elasticsearch, JavaScript, React (Next.js), etc.</p>
                             <br />
                             <div className="mt-3">
-                                <Link href="#" className="px-5 py-2 border border-white dark:border-black hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white font-medium">Read more</Link>
+                                <Link href="#" className="px-5 py-2 border border-white dark:border-black hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white font-bold">Read more</Link>
                             </div>
                         </div>
                         <div className="w-full md:w-1/3">
                             <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             <br />
-                            <h2 className="font-medium text-2xl">Amobern Pay – a modern payments solution</h2>
+                            <h2 className="font-bold text-2xl">Amobern Pay – a modern payments solution</h2>
                             <br />
                             <p className="text-gray-200 dark:text-gray-500">FinTech &nbsp;•&nbsp; Java, Spring, JavaScript, React (Next.js), AWS, etc.</p>
                             <br />
                             <div className="mt-3">
-                                <Link href="#" className="px-5 py-2 border border-white dark:border-black hover:bg-whitehover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white font-medium">Read more</Link>
+                                <Link href="#" className="px-5 py-2 border border-white dark:border-black hover:bg-whitehover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white font-bold">Read more</Link>
                             </div>
                         </div>
                         <div className="w-full md:w-1/3">
                             <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             <br />
-                            <h2 className="font-medium text-2xl">Apostle Niyi Makinde – an interactive media app</h2>
+                            <h2 className="font-bold text-2xl">Apostle Niyi Makinde – an interactive media app</h2>
                             <br />
                             <p className="text-gray-200 dark:text-gray-500">Media &nbsp;•&nbsp; Java, Spring, Elasticsearch, JavaScript, React Native (Expo CLI), etc.</p>
                             <br />
                             <div className="mt-3">
-                                <Link href="#" className="px-5 py-2 border border-white dark:border-black hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white font-medium">Read more</Link>
+                                <Link href="#" className="px-5 py-2 border border-white dark:border-black hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white font-bold">Read more</Link>
                             </div>
                         </div>
                     </div>
@@ -86,7 +86,7 @@ export default function Home() {
 
                 <section id="articles" className="flex flex-col py-[48px] pb-[56px] px-[24px] md:px-[132px] gap-[32px]">
                     <div className="flex justify-between">
-                        <p className="text-lg font-medium">ARTICLES</p>
+                        <p className="text-lg font-bold">ARTICLES</p>
                         <Link href="#">More articles</Link>
                     </div>
 
@@ -96,10 +96,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">How Computer Science is Changing the Field of Medicine</h2>
+                                <h2 className="font-bold text-2xl">How Computer Science is Changing the Field of Medicine</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -108,10 +108,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">Exploring the Intersection of Computer Science and Neuroscience</h2>
+                                <h2 className="font-bold text-2xl">Exploring the Intersection of Computer Science and Neuroscience</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -120,10 +120,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">How to Implement and Optimize Reinforcement Learning Algorithms</h2>
+                                <h2 className="font-bold text-2xl">How to Implement and Optimize Reinforcement Learning Algorithms</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -132,10 +132,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">How to Use Bayesian Networks for Probabilistic Reasoning</h2>
+                                <h2 className="font-bold text-2xl">How to Use Bayesian Networks for Probabilistic Reasoning</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -147,10 +147,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">10 Algorithms Every Computer Scientist Should Know</h2>
+                                <h2 className="font-bold text-2xl">10 Algorithms Every Computer Scientist Should Know</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -159,10 +159,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">The Impact of Computer Science on Education and Learning</h2>
+                                <h2 className="font-bold text-2xl">The Impact of Computer Science on Education and Learning</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -171,10 +171,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">The Ethics of Artificial Intelligence: Balancing Progress and Responsibility</h2>
+                                <h2 className="font-bold text-2xl">The Ethics of Artificial Intelligence: Balancing Progress and Responsibility</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
@@ -183,10 +183,10 @@ export default function Home() {
                                 <Image src="/homepage-hero_2.svg" alt="" width={568} height={408} />
                             </div>
                             <div className="flex flex-col gap-[16px] w-7/12 md:w-full">
-                                <h2 className="font-medium text-2xl">How to Build and Train Convolutional Neural Networks for Computer Vision</h2>
+                                <h2 className="font-bold text-2xl">How to Build and Train Convolutional Neural Networks for Computer Vision</h2>
                                 <p>This is an excerpt of the body of this article.</p>
                                 <div className="mt-3">
-                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-medium">Read more</Link>
+                                    <Link href="#" className="px-5 py-2 border border-black dark:border-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black font-bold">Read more</Link>
                                 </div>
                             </div>
                         </div>
